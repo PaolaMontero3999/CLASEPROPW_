4335436ef63fd9e61500ff4621ca512dea047bd1
PaolaMontero3999 <paoladc3999@gmail.com>
2025-10-29 18:59:23 +0100

    update

--- PROYECTO_INICIAL/src/Main.java
+++ PROYECTO_INICIAL/src/Main.java
@@ -1,14 +1,26 @@
 public class Main {
 
-    public static void main (String[]args){
-        System.out.println("Este es mi primer programa en Java");
-        System.out.println("Este es la segunda línea del programa ");
+
+    public static void main(String[] args) {
+        System.out.println("Primer programa en JAVA");
+        String miNombre = "Paola"; //null
+        char letraDNI = 'K';
+        letraDNI = 'P';
+        int edad = 26;
+        double altura = 1.65;
+        float peso = 62.5f;
+
+        peso = 65.8f;
+        
 
 
 
-    }
+        System.out.println("Mi nombre es:"+miNombre);
+        System.out.println("La letra de mi DNI es:"+letraDNI);
+        System.out.println("Mi edad es:"+edad);
+        System.out.println("Mi altura es:"+altura);
+        System.out.println("Mi peso es :"+peso);
 
-    public void nombreMetodo () {
 
     }
 
