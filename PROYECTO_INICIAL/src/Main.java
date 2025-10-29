23204504dc260bdc1200727fed9b5b72db734c8c
PaolaMontero3999 <paoladc3999@gmail.com>
2025-10-29 18:14:18 +0100

    update

---
+++ PROYECTO_INICIAL/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main (String[]args){
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Este es la segunda línea del programa ");
+
+
+
+    }
+
+    public void nombreMetodo () {
+
+    }
+
+
+
+
+
+}
+
