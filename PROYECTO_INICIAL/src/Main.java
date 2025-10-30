427a5b23de2cfb079e1390713bb31e335d5d85ea
PaolaMontero3999 <paoladc3999@gmail.com>
2025-10-30 15:36:34 +0100

    update

--- PROYECTO_INICIAL/src/Main.java
+++ PROYECTO_INICIAL/src/Main.java
@@ -4,29 +4,35 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Primer programa en JAVA");
         String miNombre = "Paola"; //null
+        final String DNI = "49560053K";
+
+
+        Character letraCompleja = 'K';
         char letraDNI = 'K';
+
         letraDNI = 'P';
         int edad = 26;
         double altura = 1.65;
         float peso = 62.5f;
+        boolean experiencia = true;
+
 
         peso = 65.8f;
-        
+        experiencia = false;
 
 
-
-        System.out.println("Mi nombre es:"+miNombre);
-        System.out.println("La letra de mi DNI es:"+letraDNI);
-        System.out.println("Mi edad es:"+edad);
-        System.out.println("Mi altura es:"+altura);
-        System.out.println("Mi peso es :"+peso);
+        System.out.println("Mi nombre es:" + miNombre);
+        System.out.println("La letra de mi DNI es:" + letraDNI);
+        System.out.println("Mi edad es:" + edad);
+        System.out.println("Mi altura es:" + altura);
+        System.out.println("Mi peso es :" + peso);
+        System.out.println("Mi experiencia es:" + experiencia);
+        System.out.println("El numero de mi DNI es:" + DNI);
+        System.out.println("El numero PI es:" + Math.PI);
+        System.out.println("El valor del numero E:" + Math.E);
+        System.out.println("El valor maximo de un int es:" + Integer.MAX_VALUE);
+        System.out.println("El valor minimo de un int es:" + Integer.MIN_VALUE);
 
 
     }
-
-
-
-
-
 }
-
---
+++ PROYECTO_INICIAL/src/MainMetodos.java
@@ -0,0 +1,25 @@
+public class MainMetodos  {
+    public static void main(String[] args) {
+        System.out.println("Main para probar metodos y variables");
+        saludar( "Paola");
+
+
+    }
+
+
+
+    public void saludar (String data){
+        System.out.println("Enhorabuena"+data+"has llamado a tu primer metodo");
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
